750eecca6057c5c40b62474a294e21521c0b9b11
Lefrec <blackfox244@gmail.com>
2025-01-07 16:15:45 +0100

    nombre de square dynamique au start

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var cursorPosition = [0, 0];
-var squares = [];
 var squaresPosition = [];
 const minAngle = 75;
 const bodyRange = 50;
+var squareNumber = 20;
+
+var squares = [];
+for ( let i=0; i<squareNumber; i++) {
+    let square = document.createElement("div");
+    square.style.position = "absolute";
+    square.style.height = "10px";
+    square.style.width = "10px";
+    square.style.top = "0";
+    square.style.left = "0";
+    square.style.backgroundColor = "white";
+    square.id = `square${i}`;
+    body.appendChild(square);
+    squares.push(square);
+};
 
 document.addEventListener("mousemove", getCursor);
 
@@ -12,12 +26,9 @@ function getCursor(e) {
     cursorPosition = [x, y];
 }
 
-
 setInterval(follow_cursor, 1);
 
-
 function follow_cursor() {
-    squares = [square0, square1, square2, square3, square4, square5];
     if (squares) {
         squares.forEach(function updateSquare(element, index) {
             squaresPosition[index] = [parseFloat(element.style.left), parseFloat(element.style.top)];
@@ -27,7 +38,7 @@ function follow_cursor() {
                 element.style.top = cursorPosition[1] + "px";
             } else {
                 look_at(element, squaresPosition[index], squaresPosition[index - 1])
-                lenght = Math.sqrt((squaresPosition[index][0] - squaresPosition[index - 1][0])**2 + ((squaresPosition[index][1] - squaresPosition[index - 1][1])**2));
+                let lenght = Math.sqrt((squaresPosition[index][0] - squaresPosition[index - 1][0])**2 + ((squaresPosition[index][1] - squaresPosition[index - 1][1])**2));
                 element.style.left = squaresPosition[index - 1][0] + (squaresPosition[index][0] - squaresPosition[index - 1][0]) / lenght * bodyRange + "px";
                 element.style.top = squaresPosition[index - 1][1] + (squaresPosition[index][1] - squaresPosition[index - 1][1]) / lenght * bodyRange + "px";
 
@@ -40,18 +51,18 @@ function follow_cursor() {
 }
 
 function angleConstraint(index) {
-    vectorA = [squaresPosition[index-1][0]-squaresPosition[index][0], squaresPosition[index-1][1]-squaresPosition[index][1]];
-    normA = Math.sqrt(vectorA[0]**2 + vectorA[1]**2);
-    vectorB = [squaresPosition[index + 1][0] - squaresPosition[index][0], squaresPosition[index+1][1] - squaresPosition[index][1]];
-    normB = Math.sqrt(vectorB[0]**2 + vectorB[1]**2);
-    vectorAB = [squaresPosition[index + 1][0] - squaresPosition[index - 1][0], squaresPosition[index+1][1] - squaresPosition[index-1][1]];
-    normAB = Math.sqrt(vectorAB[0]**2 + vectorAB[1]**2);
-    preAcos = (1 / 2 * (normAB**2 - normA**2 - normB**2) / (normA * normB));
-    currentAngle = (preAcos+1)*90;
+    let vectorA = [squaresPosition[index-1][0]-squaresPosition[index][0], squaresPosition[index-1][1]-squaresPosition[index][1]];
+    let normA = Math.sqrt(vectorA[0]**2 + vectorA[1]**2);
+    let vectorB = [squaresPosition[index + 1][0] - squaresPosition[index][0], squaresPosition[index+1][1] - squaresPosition[index][1]];
+    let normB = Math.sqrt(vectorB[0]**2 + vectorB[1]**2);
+    let vectorAB = [squaresPosition[index + 1][0] - squaresPosition[index - 1][0], squaresPosition[index+1][1] - squaresPosition[index-1][1]];
+    let normAB = Math.sqrt(vectorAB[0]**2 + vectorAB[1]**2);
+    let preAcos = (1 / 2 * (normAB**2 - normA**2 - normB**2) / (normA * normB));
+    let currentAngle = (preAcos+1)*90;
     if (currentAngle < minAngle) {
-        deltaAngle = (minAngle-currentAngle) *(Math.PI/180);
+        let deltaAngle = (minAngle-currentAngle) *(Math.PI/180);
         deltaAngle *= Math.sign(vectorA[0]*vectorB[1]-vectorA[1]*vectorB[0]);
-        newVectorB = [Math.cos(deltaAngle)*vectorB[0]+(-Math.sin(deltaAngle))*vectorB[1] , Math.sin(deltaAngle)*vectorB[0]+(Math.cos(deltaAngle))*vectorB[1]];
+        let newVectorB = [Math.cos(deltaAngle)*vectorB[0]+(-Math.sin(deltaAngle))*vectorB[1] , Math.sin(deltaAngle)*vectorB[0]+(Math.cos(deltaAngle))*vectorB[1]];
         squares[index+1].style.left = squaresPosition[index][0]+newVectorB[0]+"px";
         squares[index+1].style.top = squaresPosition[index][1]+newVectorB[1]+"px";
     }
@@ -59,4 +70,18 @@ function angleConstraint(index) {
 
 function look_at(element, position, target) {
     element.style.rotate = Math.atan2(target[1] - position[1], target[0] - position[0]) + "rad";
+}
+
+function grow() {
+    console.log("growing");
+    document.getElementById("body").innerHTML += 
+    `<div style="
+    position: absolute;
+    height: 10px;
+    width: 10px;
+    top: 0;
+    left: 0;
+    background-color: white;" id="square${squareNumber}">
+    </div>`;
+    squares.push(document.getElementById(`square${squareNumber}`));
 }
